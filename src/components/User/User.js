cf8d1810905888e456e8220100bb676e71405e2c
sean <seanschmidt89@yahoo.com>
2023-01-05 05:03:17 -0800

    finised rating system for user

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { BiStar } from "react-icons/bi";
 import "./User.css";
 
 const User = ({ user }) => {
+  let rating = Math.ceil(Math.random() * (10 - 4) + 4);
   return (
     <div className="user">
       <img src={user.picture.medium} alt={user.name.first} />
       <p>{user.name.first}</p>
+      <div className="rating">
+        <p>{rating}</p>
+        <BiStar />
+      </div>
     </div>
   );
 };
