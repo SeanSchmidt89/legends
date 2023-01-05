d65022b34797f0a7f69627ac4f650330f8f7d25b
sean <seanschmidt89@yahoo.com>
2023-01-05 02:09:23 -0800

    adding data from api to comps

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,19 +1,29 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../../store/userSlice";
+import User from "../User/User";
 import axios from "axios";
 import "./Reviews.css";
 
 const Reviews = () => {
   const dispatch = useDispatch();
-  const userList = useSelector((state) => state.users.users)
+  const userList = useSelector((state) => state.users.users);
   useEffect(() => {
     axios
       .get("https://randomuser.me/api/?results=10")
       .then((response) => dispatch(getUsers(response.data.results)))
       .catch((error) => console.log(error.massage));
   }, [dispatch]);
-  return <div className="reviews">Reviews</div>;
+  return (
+    <div className="reviews">
+      Reviews
+      <div className="container">
+        {userList.map((item) => (
+          <User key={item.id} user={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./User.css";
 
-const User = () => {
-  return <div>User</div>;
+const User = ({ user }) => {
+  return (
+    <div className="user">
+      <img src={user.picture.medium} alt={user.name.first} />
+      <p>{user.name.first}</p>
+    </div>
+  );
 };
 
 export default User;
