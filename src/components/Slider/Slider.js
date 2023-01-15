c8e37d4d22146fd1db6b09e29f779288f8c7ea82
sean <seanschmidt89@yahoo.com>
2023-01-15 00:50:35 -0800

    adding imgs to wynwood comp

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -5,13 +5,8 @@ import {
 } from "react-icons/md";
 import "./Slider.css";
 
-const Slider = () => {
+const Slider = ({ imgArr }) => {
   let [imgNum, setImgNum] = useState(0);
-  let imgArr = [
-    "https://images.unsplash.com/photo-1548209454-8548045387bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2335&q=80",
-    "https://images.unsplash.com/photo-1608135003575-9b587f3b0cf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
-    "https://images.unsplash.com/photo-1590477329022-dffe7d88c970?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
-  ];
 
   const backHandler = () => {
     if (imgNum === 0) {
@@ -22,7 +17,7 @@ const Slider = () => {
   };
 
   const forwardHandler = () => {
-    if (imgNum === 2) {
+    if (imgNum === 4) {
       setImgNum(0);
     } else {
       setImgNum(imgNum + 1);
@@ -31,6 +26,7 @@ const Slider = () => {
   return (
     <div className="slider">
       <img className="slider-img" src={imgArr[imgNum]} alt="/" />
+      <div className="slider-overlay" />
       <MdOutlineArrowBackIos className="back" onClick={backHandler} size={60} />
       <MdOutlineArrowForwardIos
         className="forward"
--- src/routes/Wynwood/Wynwood.js
+++ src/routes/Wynwood/Wynwood.js
@@ -3,10 +3,16 @@ import Slider from "../../components/Slider/Slider";
 import "./Wynwood.css";
 
 const Wynwood = () => {
+  let imgArr = [
+    "https://images.unsplash.com/photo-1590477329022-dffe7d88c970?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    "https://images.unsplash.com/photo-1593839745217-d514b8821450?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3870&q=80",
+    "https://images.unsplash.com/photo-1621369929981-a2ca4fa55f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
+    "https://images.unsplash.com/photo-1608135003575-9b587f3b0cf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    "https://images.unsplash.com/photo-1529326353831-df2fd0f44a9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+  ];
   return (
     <div>
-      Wynwood
-      <Slider />
+      <Slider imgArr={imgArr} />
     </div>
   );
 };
