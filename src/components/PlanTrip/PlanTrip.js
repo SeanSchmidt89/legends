2951535d1ba41e15fa8c159a374870e3006e6ca5
sean <seanschmidt89@yahoo.com>
2023-01-19 07:13:25 -0800

    css on several comps

--- src/components/PlanTrip/PlanTrip.js
+++ src/components/PlanTrip/PlanTrip.js
@@ -17,11 +17,9 @@ const PlanTrip = () => {
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat.
           </p>
         </div>
-
         <div className="plan-container">
           <img
             className="img-one"
