f2be7a2d0d67619405820976ee274baa724235ca
sean <seanschmidt89@yahoo.com>
2023-01-25 04:53:03 -0800

    fixed side nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
+  const [sideNav, setSideNav] = useState(false);
+
+  const sideNavHandler = () => {
+    setSideNav(!sideNav);
+  };
   return (
     <div className="nav">
       <Link to="/">
         <h1>Legends</h1>
       </Link>
-      <div className="nav-links">
+      <div className={!sideNav ? "nav-links" : "side-nav"}>
         <ul>
-          <Link to="/">
+          <Link onClick={sideNavHandler} to="/">
             <li>Home</li>
           </Link>
-          <Link to="/">
+          <Link onClick={sideNavHandler} to="/">
             <li>Catalog</li>
           </Link>
-          <Link to="/">
+          <Link onClick={sideNavHandler} to="/">
             <li>About</li>
           </Link>
         </ul>
       </div>
-      <RxHamburgerMenu className="hamburger" size={22} />
+      {!sideNav ? (
+        <RxHamburgerMenu
+          className="hamburger"
+          onClick={sideNavHandler}
+          size={22}
+        />
+      ) : (
+        <AiOutlineClose className="x" onClick={sideNavHandler} size={22} />
+      )}
     </div>
   );
 };
--- src/routes/LittleHavana/LittleHavana.js
+++ src/routes/LittleHavana/LittleHavana.js
@@ -6,8 +6,8 @@ import "./LittleHavana.css";
 
 const LittleHavana = () => {
   let imgArr = [
-    "https://images.unsplash.com/photo-1558294417-831b4c991662?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80",
     "https://images.unsplash.com/photo-1624477836421-670f3ed080fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80",
+    "https://images.unsplash.com/photo-1558294417-831b4c991662?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80",
     "https://images.unsplash.com/photo-1570396662990-ec320a4d6ce7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80",
     "https://images.unsplash.com/photo-1558294417-4ab8762c8ae8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80",
     "https://images.unsplash.com/photo-1541293712104-77db9e7752ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
