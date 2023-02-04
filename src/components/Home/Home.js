40d90b7b16b4b9051adcdfd82fbbf7c617fb305e
sean <seanschmidt89@yahoo.com>
2023-02-04 05:02:01 -0800

    css fixes to hero comp

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
-// import Hero from "../Hero/Hero";
+import Hero from "../Hero/Hero";
 import Reviews from "../Reviews/Reviews";
 import PhotoGrid from "../PhotoGrid/PhotoGrid";
 import AreaLinks from "../AreaLinks/AreaLinks";
-// import PlanTrip from "../PlanTrip/PlanTrip";
+import PlanTrip from "../PlanTrip/PlanTrip";
 import Footer from "../Footer/Footer";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
-      {/* <Hero />
-      <PlanTrip /> */}
+      <Hero />
+      <PlanTrip />
       <AreaLinks />
       <PhotoGrid />
       <Reviews />
